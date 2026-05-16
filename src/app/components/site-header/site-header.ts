5d88673abd7a63d5fe6e2040ab038736ad9c7878
LuizOtavioTG <bc.luizo@gmail.com>
2026-05-15 21:40:18 -0300

    style: add scrolled styles to site header

--- src/app/components/site-header/site-header.ts
+++ src/app/components/site-header/site-header.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostBinding, NgZone, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-site-header',
   templateUrl: './site-header.html',
   styleUrl: './site-header.scss',
 })
-export class SiteHeader {}
+export class SiteHeader implements AfterViewInit, OnDestroy {
+  @HostBinding('class.is-scrolled') protected isScrolled = false;
+
+  private readonly scrollThreshold = 28;
+  private removeScrollListener?: () => void;
+
+  constructor(private readonly ngZone: NgZone) {}
+
+  ngAfterViewInit(): void {
+    this.ngZone.runOutsideAngular(() => {
+      const updateHeaderState = (): void => {
+        const nextIsScrolled = window.scrollY > this.scrollThreshold;
+
+        if (nextIsScrolled === this.isScrolled) {
+          return;
+        }
+
+        this.ngZone.run(() => {
+          this.isScrolled = nextIsScrolled;
+        });
+      };
+
+      window.addEventListener('scroll', updateHeaderState, { passive: true });
+      updateHeaderState();
+
+      this.removeScrollListener = () => window.removeEventListener('scroll', updateHeaderState);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.removeScrollListener?.();
+  }
+}
